1166b33a56c0bf2130948456098814e6718cc840
Nathaniel Motley <nmotley92@gmail.com>
2023-03-04 02:00:42 -0600

    Adds weppack config

---
+++ client/src/js/database.js
@@ -0,0 +1,31 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+  await db.put('jate', content);
+  console.log('Content added to database');
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+
+export const getDb = async () => {
+  const db = await initdb();
+  const content = await db.getAll('jate');
+  console.log('Content retrieved from database');
+  return content;
+};
+
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,75 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+
+      new InjectManifest({
+        swSrc: './src/js/sw.js', // Path to your service worker file
+        swDest: 'sw.js', // Name of the generated service worker file
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        name: 'Jate\'s PWA',
+        short_name: 'jate',
+        description: 'Just another text editor',
+        background_color: '#01579b',
+        theme_color: '#ffffff',
+        'theme-color': '#ffffff',
+        start_url: '../index.html',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: '1024x1024',
+            ios: true,
+          },
+        ],
+      }),
+     
+    ],
+
+
+    module: {
+      rules: [
+        // TODO: Add CSS loaders and babel to webpack.
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
