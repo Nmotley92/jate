b24ba80e507a2a8266b4f685408fe576d625ef2c
Nathaniel Motley <nmotley92@gmail.com>
2023-03-05 02:02:57 -0600

    revert changes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app', 'index.html'));
-});
+require('./routes/htmlRoutes')(app);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running');
-});
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
