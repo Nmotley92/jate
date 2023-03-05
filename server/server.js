4eadf5b4987124a3afd8edd135868680de4ce6f9
Nathaniel Motley <nmotley92@gmail.com>
2023-03-05 01:54:02 -0600

    changes server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'app')));
 
-require('./routes/htmlRoutes')(app);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running');
+});
