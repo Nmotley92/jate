eebaaf9e19a654125b858f5e6e26991d44f8a141
Nathaniel Motley <nmotley92@gmail.com>
2023-03-08 18:46:19 -0600

    removes key

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,12 @@ const initdb = async () =>
     },
   });
   
-// puts the content in the database but uses put instead of add so it will update the last item in the database
+// puts the content in the database 
 export const putDb = async (content) => {
   const db = await initdb();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  await store.put({ content }, 1);
+  await store.put({ content });
   await tx.complete;
   console.log('Content added to database');
 }
